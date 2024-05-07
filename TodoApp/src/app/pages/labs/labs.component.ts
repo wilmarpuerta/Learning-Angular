ed6982be84b1607498575b8e7c618524ea1eda95
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-07 02:25:37 -0500

    Creación de componentes, enrutamientos

---
+++ TodoApp/src/app/pages/labs/labs.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-labs',
+  standalone: true,
+  imports: [],
+  templateUrl: './labs.component.html',
+  styleUrl: './labs.component.css'
+})
+export class LabsComponent {
+  welcome = "hola";
+  tasks = [
+    'Instalar el angular CLI',
+    'Crear proyecto',
+    'Crear componentes'
+  ]
+}
