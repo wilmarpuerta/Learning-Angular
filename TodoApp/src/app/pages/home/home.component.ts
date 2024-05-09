3037961f4669e4a81247fa035388fc6111d31a6a
Wilmar Puerta <wilmarandrespuertajaraillo@gmail.com>
2024-05-09 01:42:01 -0500

    List tasks in the home

--- TodoApp/src/app/pages/home/home.component.ts
+++ TodoApp/src/app/pages/home/home.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-
+  tasks = signal([
+    'Instalar el angular CLI',
+    'Crear proyecto',
+    'Crear componentes'
+  ])
 }
